96c3453ddd2fba8274201558ed81d24cfa5c020e
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-13 10:41:07 -0300

    feat: added config to include routes in router

--- src/app/router/router.go
+++ src/app/router/router.go
@@ -1,8 +1,12 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"devbook-api/src/app/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 // GenerateRouter returns a router with routes configured
 func GenerateRouter() *mux.Router {
-	return mux.NewRouter()
+	return routes.ConfigureRoutes(mux.NewRouter())
 }
---
+++ src/app/router/routes/routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Route represent routes of API
+type Route struct {
+	Uri       string
+	Method    string
+	Handler   func(http.ResponseWriter, *http.Request)
+	IsPrivate bool
+}
+
+func ConfigureRoutes(router *mux.Router) *mux.Router {
+	routes := UserRoutes
+
+	for _, route := range routes {
+		router.HandleFunc(route.Uri, route.Handler).Methods(route.Method)
+	}
+
+	return router
+}
