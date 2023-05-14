2e6ec2acc290827ec067d11ea7a81ea652d82c1e
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-14 10:15:25 -0300

    feat: added logic to UserFindAllController

--- src/app/controllers/user/findAll_controller.go
+++ src/app/controllers/user/findAll_controller.go
@@ -1,7 +1,30 @@
 package controller
 
-import "net/http"
+import (
+	presenter "devbook-api/src/app/presenters"
+	"devbook-api/src/infra/database"
+	repository "devbook-api/src/infra/database/repositories/user"
+	"net/http"
+	"strings"
+)
 
 func UserFindAllController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Encontrando todos os Usuário!"))
+	nameOrNickName := strings.ToLower(r.URL.Query().Get("user"))
+
+	db, err := database.GetDbConnection()
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	userRepository := repository.GetUserRepository(db)
+
+	users, err := userRepository.FindAll(nameOrNickName)
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	presenter.ReponsePresenter(w, http.StatusOK, users)
 }
