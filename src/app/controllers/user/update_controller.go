350bd3e0e4e6d5b2c6e97334ca7791eb6cd8d860
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-14 11:16:27 -0300

    feat: added logic to UserUpdateController

--- src/app/controllers/user/update_controller.go
+++ src/app/controllers/user/update_controller.go
@@ -1,7 +1,38 @@
 package controller
 
-import "net/http"
+import (
+	presenter "devbook-api/src/app/presenters"
+	"devbook-api/src/infra/database"
+	repository "devbook-api/src/infra/database/repositories/user"
+	"net/http"
+)
 
 func UserUpdateController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Atualizando um Usuário!"))
+	userId, err := GetUserId(r)
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusBadRequest, err)
+		return
+	}
+
+	user, err, statusCode := GetUserFromBody(r, true)
+	if err != nil {
+		presenter.ErrorPresenter(w, statusCode, err)
+		return
+	}
+
+	db, err := database.GetDbConnection()
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	userRepository := repository.GetUserRepository(db)
+
+	if err = userRepository.Update(userId, user); err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	presenter.ReponsePresenter(w, http.StatusNoContent, nil)
 }
