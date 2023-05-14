2640b38ad1ecfaca2a036c03dcce1e76d4aef208
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-14 10:13:55 -0300

    feat: added FindAll method to UserRepository

--- src/infra/database/repositories/user/user_repository.go
+++ src/infra/database/repositories/user/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	model "devbook-api/src/domain/models"
+	"fmt"
 )
 
 type userRepository struct {
@@ -33,4 +34,35 @@ func (userRepository userRepository) Create(user model.User) (uint64, error) {
 	}
 
 	return uint64(userId), nil
+}
+
+func (userRepository userRepository) FindAll(nameOrNickName string) ([]model.User, error) {
+	nameOrNickName = fmt.Sprintf("%%%s%%", nameOrNickName)
+
+	var users []model.User
+	rows, err := userRepository.db.Query(
+		"SELECT id, name, nickName, email, createdAt FROM users WHERE name LIKE ? OR nickName LIKE ?",
+		nameOrNickName,
+		nameOrNickName,
+	)
+	if err != nil {
+		return users, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user model.User
+		if err = rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.NickName,
+			&user.Email,
+			&user.CreatedAt); err != nil {
+			return users, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
