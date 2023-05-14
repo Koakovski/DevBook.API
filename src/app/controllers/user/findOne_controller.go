bda5f23b3a7043f5e0d945150e03bdd3fef2ffb8
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-14 10:28:48 -0300

    feat: added logic to UserFindOneController

--- src/app/controllers/user/findOne_controller.go
+++ src/app/controllers/user/findOne_controller.go
@@ -1,7 +1,38 @@
 package controller
 
-import "net/http"
+import (
+	presenter "devbook-api/src/app/presenters"
+	"devbook-api/src/infra/database"
+	repository "devbook-api/src/infra/database/repositories/user"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
 
 func UserFindOneController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Encontrando um Usuário!"))
+	params := mux.Vars(r)
+
+	userId, err := strconv.ParseUint(params["id"], 10, 64)
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.GetDbConnection()
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	userRepository := repository.GetUserRepository(db)
+
+	user, err := userRepository.FindById(userId)
+	if err != nil {
+		presenter.ErrorPresenter(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	presenter.ReponsePresenter(w, http.StatusOK, user)
 }
