85b78cbe592e765fb949842f8e23d834a8461e41
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-14 11:15:49 -0300

    fix: fixed Response presenter to handle nil values

--- src/app/presenters/response_presenter.go
+++ src/app/presenters/response_presenter.go
@@ -10,7 +10,9 @@ func ReponsePresenter(w http.ResponseWriter, statusCode int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Fatal(err)
+	if data != nil {
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
