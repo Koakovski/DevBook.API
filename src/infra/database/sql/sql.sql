79e49b698bb31c17511e6ebd125b2fe2e636661a
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-16 10:49:01 -0300

    feat: added followers table

--- src/infra/database/sql/sql.sql
+++ src/infra/database/sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -10,4 +11,13 @@ CREATE TABLE users(
     email varchar(50) not null unique,
     password varchar(100) not null,
     createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
+
+CREATE TABLE followers(
+    userId int not null,
+    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+    followerId int not null,
+    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+
+    primary key (userId, followerId)
 ) ENGINE=INNODB;
