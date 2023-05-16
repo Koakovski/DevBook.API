a46f44a3dcb9201985677772a604efb2a31f97e0
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-16 10:01:39 -0300

    feat: added token validation to middleware

--- src/app/middlewares/middlwares.go
+++ src/app/middlewares/middlwares.go
@@ -1,9 +1,18 @@
 package middlwares
 
-import "net/http"
+import (
+	presenter "devbook-api/src/app/presenters"
+	"devbook-api/src/infra/auth"
+	"net/http"
+)
 
 func Authenticate(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if err := auth.ValidateToken(r); err != nil {
+			presenter.ErrorPresenter(w, http.StatusUnauthorized, err)
+			return
+		}
+
 		next(w, r)
 	}
 }
--- src/infra/auth/auth.go
+++ src/infra/auth/auth.go
@@ -2,6 +2,10 @@ package auth
 
 import (
 	"devbook-api/src/infra/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -16,4 +20,38 @@ func CreateToken(userId uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 
 	return token.SignedString(config.SecretKey)
+}
+
+func ValidateToken(r *http.Request) error {
+	tokenString := extractToken(r)
+
+	token, err := jwt.Parse(tokenString, getVerificationKey)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("invalid token")
+}
+
+func extractToken(r *http.Request) string {
+	authorizationString := r.Header.Get("Authorization")
+
+	token := strings.Split(authorizationString, " ")
+	if len(token) == 2 {
+		return token[1]
+	}
+
+	return ""
+}
+
+func getVerificationKey(token *jwt.Token) (any, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signature method. %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
